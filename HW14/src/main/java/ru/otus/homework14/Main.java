5a2adfae91145e6d9a081351e0b0437d65be3f56
Alexander Strelchenko <furion1gg@gmail.com>
2018-02-14 22:00:43 +0300

    Fix handle different number of threads

--- HW14/src/main/java/ru/otus/homework14/Main.java
+++ HW14/src/main/java/ru/otus/homework14/Main.java
@@ -2,7 +2,8 @@ package ru.otus.homework14;
 
 
 public class Main {
-    public static final int SIZE_OF_ARRAY = 16;
+    public static final int SIZE_OF_ARRAY = 18;
+    public static final int NUMBER_OF_THREADS = 3;
 
     public static void main(String[] args) {
         int[] array = new int[SIZE_OF_ARRAY];
@@ -12,7 +13,7 @@ public class Main {
 
         System.out.println("Before sort: ");
         Sorter.printArray(array);
-        int[] sortedArray = Sorter.multiThreadSortArray(array);
+        int[] sortedArray = Sorter.multiThreadSortArray(array, NUMBER_OF_THREADS);
         System.out.println("After sort: ");
         Sorter.printArray(sortedArray);
     }
--- HW14/src/main/java/ru/otus/homework14/Sorter.java
+++ HW14/src/main/java/ru/otus/homework14/Sorter.java
@@ -3,83 +3,6 @@ package ru.otus.homework14;
 import java.util.Arrays;
 
 public class Sorter extends Thread {
-    public static int[] multiThreadSortArray(int[] inputArray){
-        int[] array = new int[inputArray.length];
-        int[] outputArray = new int[inputArray.length];
-        System.arraycopy(inputArray, 0, array, 0, inputArray.length);
-        int lengthOfChunk = array.length/4;
-
-        int[] firstArray = new int[lengthOfChunk];
-        int[] secondArray = new int[lengthOfChunk];
-        int[] thirdArray = new int[lengthOfChunk];
-        int[] fourthArray = new int[lengthOfChunk];
-
-        System.arraycopy(array, 0, firstArray, 0, lengthOfChunk);
-        System.arraycopy(array, lengthOfChunk, secondArray, 0, lengthOfChunk);
-        System.arraycopy(array, lengthOfChunk * 2, thirdArray, 0, lengthOfChunk);
-        System.arraycopy(array, lengthOfChunk * 3, fourthArray, 0, lengthOfChunk);
-
-        Sorter sorter1 = new Sorter(firstArray);
-        Sorter sorter2 = new Sorter(secondArray);
-        Sorter sorter3 = new Sorter(thirdArray);
-        Sorter sorter4 = new Sorter(fourthArray);
-
-        try {
-            sorter1.start();
-            sorter1.join();
-            sorter2.start();
-            sorter2.join();
-            sorter3.start();
-            sorter3.join();
-            sorter4.start();
-            sorter4.join();
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
-
-        int firstIndex = 0, secondIndex = 0, thirdIndex = 0, fourthIndex = 0;
-        for(int i = 0; i < outputArray.length; i++){
-            int firstValue = getValueFromArrayOrIntegerMaxValue(firstIndex, firstArray);
-            int secondValue = getValueFromArrayOrIntegerMaxValue(secondIndex, secondArray);
-            int thirdValue = getValueFromArrayOrIntegerMaxValue(thirdIndex, thirdArray);
-            int fourthValue = getValueFromArrayOrIntegerMaxValue(fourthIndex, fourthArray);
-
-            int min = findMin(firstValue, secondValue, thirdValue, fourthValue);
-            outputArray[i] = min;
-            if(min == firstValue){
-                firstIndex++;
-            }else if(min == secondValue){
-                secondIndex++;
-            }else if(min == thirdValue){
-                thirdIndex++;
-            }else{
-                fourthIndex++;
-            }
-        }
-
-        return outputArray;
-    }
-
-    private static int getValueFromArrayOrIntegerMaxValue(int index, int[] array){
-        if(index < array.length){
-            return array[index];
-        }else{
-            return Integer.MAX_VALUE;
-        }
-    }
-
-    private static int findMin(int firstValue, int secondValue, int thirdValue, int fourthValue){
-        if(firstValue <= secondValue && firstValue <= thirdValue && firstValue <= fourthValue){
-            return firstValue;
-        }else if(secondValue <= firstValue && secondValue <= thirdValue && secondValue <= fourthValue){
-            return secondValue;
-        }else if(thirdValue <= firstValue && thirdValue <= secondValue && thirdValue <= fourthValue){
-            return thirdValue;
-        }else{
-            return fourthValue;
-        }
-    }
-
     private int[] array;
     public Sorter(int[] inputArray){
         this.array = inputArray;
@@ -102,4 +25,73 @@ public class Sorter extends Thread {
         Arrays.sort(array);
     }
 
+    public static int[] multiThreadSortArray(int[] inputArray, int numberOfThreads){
+        int[] outputArray = new int[inputArray.length];
+
+        int lengthOfChunk = inputArray.length / numberOfThreads;
+        int lengthOfLastChunk = lengthOfChunk + (inputArray.length % numberOfThreads);
+
+        int[][] allArrays = new int[numberOfThreads][];
+        for (int i = 0; i < numberOfThreads; i++) {
+            if (i != numberOfThreads - 1) {
+                allArrays[i] = new int[lengthOfChunk];
+                System.arraycopy(inputArray, i * lengthOfChunk, allArrays[i], 0, lengthOfChunk);
+            } else {
+                allArrays[i] = new int[lengthOfLastChunk];
+                System.arraycopy(inputArray, i * lengthOfChunk, allArrays[i], 0, lengthOfLastChunk);
+            }
+        }
+
+        Sorter[] sorters = new Sorter[numberOfThreads];
+        for(int i = 0; i < numberOfThreads; i++){
+            sorters[i] = new Sorter(allArrays[i]);
+        }
+
+        try {
+            for (Sorter s : sorters) {
+                s.start();
+                s.join();
+            }
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        int[] indexes = new int[numberOfThreads];
+        for(int i = 0; i < indexes.length; i++){
+            indexes[i] = 0;
+        }
+
+        for(int i = 0; i < outputArray.length; i++){
+            int[] minValues = makeArrayOfMinValues(allArrays, indexes);
+            int indexOfMinValue = findIndexOfMin(minValues);
+            indexes[indexOfMinValue]++;
+            outputArray[i] = minValues[indexOfMinValue];
+        }
+
+        return outputArray;
+    }
+
+    private static int[] makeArrayOfMinValues(int[][] allArrays, int[] indexes){
+        int[] result = new int[allArrays.length];
+        for(int i = 0; i < allArrays.length; i++){
+            if(indexes[i] >= allArrays[i].length){
+                result[i] = Integer.MAX_VALUE;
+            }else {
+                result[i] = allArrays[i][indexes[i]];
+            }
+        }
+        return result;
+    }
+
+    private static int findIndexOfMin(int[] values){
+        int min = values[0];
+        int index = 0;
+        for(int i = 0; i < values.length; i++){
+            if(values[i] < min){
+                min = values[i];
+                index = i;
+            }
+        }
+        return index;
+    }
 }
